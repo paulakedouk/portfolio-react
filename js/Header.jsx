56ee73f1de35f6f33902ff9588afde2507ebe103
Paula Kedouk <paulakedouk@gmail.com>
2017-06-09 15:35:06 -0700

    Add new component Header

--- js/Header.jsx
+++ js/Header.jsx
@@ -1,63 +1,18 @@
 import React from 'react';
-
-const TextHeader = {
-  margin: '0 auto',
-  paddingBottom: '100px',
-  alignContent: 'center'
-};
-
-const Title = {
-  fontSize: '60px',
-  fontWeight: '700',
-  color: 'white',
-  textAlign: 'center',
-  marginBottom: '15px'
-};
-
-const Subtitle = {
-  backgroundColor: '#ED527F',
-  color: 'white',
-  paddingTop: '10px',
-  width: '340px',
-  height: '30px',
-  borderRadius: '20px',
-  letterSpacing: '3px',
-  fontWeight: '300',
-  fontSize: '14px',
-  textAlign: 'center',
-  margin: '0 auto'
-};
-
-const Quote = {
-  position: 'absolute',
-  left: '0',
-  right: '0',
-  bottom: '70px',
-  textAlign: 'center',
-  color: 'white',
-  letterSpacing: '2px',
-  fontSize: '16px'
-};
-
-const Arrow = {
-  width: '50px',
-  position: 'absolute',
-  left: '0',
-  right: '0',
-  bottom: '30px',
-  margin: '0 auto',
-  cursor: 'pointer',
-  transform: 'matrix(1, 0, 0, 1, 0, 0)'
-};
+import preload from '../data.json';
 
 const Header = () => (
-  <div className="background">
-    <div style={TextHeader}>
-      <h1 style={Title}>anapaula.io</h1>
-      <h2 style={Subtitle}>designer & developer</h2>
-      <p style={Quote}>Testando</p>
-      <div className="arrowImg" style={Arrow} />
-    </div>
+  <div className="background main">
+    {preload.header.map(infos => (
+      <div className="header-text">
+        <h1 className="header-title">{infos.name}</h1>
+        <h2 className="header-subtitle">{infos.subtitle}</h2>
+        <p className="header-p">{infos.text}</p>
+        <div className="header-arrow">
+          <img alt="Arrow" src={`../public/img/arrow.svg`} />
+        </div>
+      </div>
+    ))}
   </div>
 );
 
